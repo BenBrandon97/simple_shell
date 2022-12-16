7aaccb3c449a4d6fa098e925613768f0eb28ecc2
Ben Brandon/ <benbrandon97@gmail.com>
2022-12-16 15:41:17 +0300

    Update tokenizer.c
    Co-Authored-By: KateMwende <113633390+KateMwende@users.noreply.github.com>

--- tokenizer.c
+++ tokenizer.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * **strtow - splits a string into words. Repeat delimiters are ignored
+ * **strtow - splits a string into words, ignored repeat delimiters
  * @str: the input string
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
@@ -47,7 +47,6 @@ char **strtow(char *str, char *d)
 	s[j] = NULL;
 	return (s);
 }
-
 /**
  * **strtow2 - splits a string into words
  * @str: the input string
